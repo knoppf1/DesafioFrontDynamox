2493417b8fe5337f496556ddc025a06705da3bee
knoppf1 <kerstnerfabiano@gmail.com>
2022-10-12 20:48:01 -0300

    Lançamento aplicação

--- DesafioFrontDynamox/src/app/app.module.ts
+++ DesafioFrontDynamox/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+import { SensoresComponent } from './sensores/sensores.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatoComponent,
+    SensoresComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
+
     ReactiveFormsModule,
     AppRoutingModule
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
